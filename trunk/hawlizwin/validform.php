fbddabbde1460c66cf1ea930017c624274c4a31a
akramfares@gmail.com <akramfares@gmail.com@8b7d46ad-3e48-131e-2f50-54cab80759d4>
2013-10-11 16:41:03 +0000

    Ajout de facebook login

--- trunk/hawlizwin/validform.php
+++ trunk/hawlizwin/validform.php
@@ -1,4 +1,5 @@
 <?php include("src/utils.php"); ?>
+<?php include("fb-login.php"); ?>
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -9,16 +10,39 @@
 </head>
 <body>
 <?php
+    print_r($user_profile);
+
     if($_POST){
+
+        $nom = $user_profile["name"];
+        $email = $user_profile["email"];
+        $username = $user_profile["username"];
+        $photo = "https://graph.facebook.com/".$user."/picture";
+        $ville = $user_profile["location"]["name"];
+        if($user_profile["gender"] == "male") $sexe = 0;
+        else $sexe = 1;
+        $ip = $_SERVER["REMOTE_ADDR"];
+
+        $stmt = $dbh->prepare("INSERT INTO `users` (`id`, `email`, `oauth_uid`, `oauth_provider`, `username`, `twitter_oauth_token`, `twitter_oauth_token_secret`, `nom`, `sexe`, `photo`, `ville`, `date`, `ip`) VALUES (NULL, '$email', NULL, NULL, '$username', NULL, NULL, '$nom', '$sexe', '$photo', '$ville', CURRENT_TIMESTAMP, '$ip');");
+        $stmt->execute();
+
+        $sth = $dbh->prepare("SELECT * FROM users WHERE email ='$email'");
+        $sth->execute();
+        $user = $sth->fetch();
+        $userid = $user["id"];
+
         $photo = $_POST["photo"];
         $prix = $_POST["prix"];
         $ville = $_POST["ville"];
         $description = $_POST["description"];
 
-        $stmt = $dbh->prepare("INSERT INTO  `hawlizwin`.`photos` (`id` , `userid` , `url` , `prix` , `ville` , `description` , `vote` , `date` ) VALUES (NULL ,  '2',  '$photo',  '$prix',  '$ville',  '$description',  '', CURRENT_TIMESTAMP );"); 
+
+
+        $stmt = $dbh->prepare("INSERT INTO  `photos` (`id` , `userid` , `url` , `prix` , `ville` , `description` , `vote` , `date` ) VALUES (NULL ,  '$userid',  '$photo',  '$prix',  '$ville',  '$description',  '', CURRENT_TIMESTAMP );"); 
 
         $stmt->execute();
 
+        
     }
 
 ?>
