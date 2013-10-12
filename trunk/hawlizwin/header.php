2dd505ca7cce8adcaeb1fa54cbb83bfa5ac8bc46
akramfares@gmail.com <akramfares@gmail.com@8b7d46ad-3e48-131e-2f50-54cab80759d4>
2013-10-12 18:48:25 +0000

    Modification du menu

--- trunk/hawlizwin/header.php
+++ trunk/hawlizwin/header.php
@@ -7,11 +7,15 @@
         </div>
         
         <nav id="menu" class="nav">
-			<ul id="top-menu" <?php if($active_accueil) echo 'style="margin-top: 5.1%;"' ?>>
+			<ul id="top-menu">
 			    <li><a href="index.php" class="<?php if($active_accueil) echo "active"; ?>">Accueil</a></li>
                 <li><a href="ajouter.php" class="<?php if($active_participer) echo "active"; ?> participer">Participer</a></li>
                 <li><a href="list.php" class="<?php if($active_galerie) echo "active"; ?> galerie">Galerie</a></li>
-                <li><a href="list.php" id="social">@</a></li>
+                
+                <li>
+                    <a href="list.php" id="social"><img src="img/social_btn.png"></a>
+                    <li>test</li>
+                </li>
 			</ul>
             <div class="clear"></div>
 		</nav>
