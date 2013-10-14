2066442b99aa95cb5ea54ede99e724359b834623
cheick.ismo@gmail.com <cheick.ismo@gmail.com@8b7d46ad-3e48-131e-2f50-54cab80759d4>
2013-10-14 22:15:00 +0000

    Modif header.php Ajouter du modal sur l'image avec le lien de la vidéos

--- trunk/hawlizwin/header.php
+++ trunk/hawlizwin/header.php
@@ -11,22 +11,11 @@
                 <li><a href="index.php" class="<?php if($active_accueil) echo "active"; ?>">Accueil</a></li>
                 <li><a href="ajouter.php" class="<?php if($active_participer) echo "active"; ?> participer">Participer</a></li>
                 <li><a href="list.php" class="<?php if($active_galerie) echo "active"; ?> galerie">Galerie</a></li>
-                
                 <li>
                     
-                   <a href="#" id="example" rel="popover"  ><img src="img/social_btn.png"></a>
+                   <a href="list.php" id="social" ><img src="img/social_btn.png"></a>
                 
-                 <script>
-				  
-				 $('#example').popover({
-				  placement: 'bottom',
-				  title: 'Partager ',
-				  html: true,
-				  content: '<button class="btn"><a name="fb_share" type="box_count" share_url="http://7awlizwin.com/index.php">Partager</a></button>'
-				});
-                </script>
-                
-                <script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+
  
                 </li>
             </ul>
